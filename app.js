d92d0daeeeb7a022087e822985f33d2bff1053b9
Ashley Herrera <ashleyherrera131@gmail.com>
2021-02-17 09:38:36 -0700

    function

---
+++ app.js
@@ -0,0 +1,20 @@
+// Examples
+  // 'taco cat' => true
+  // 'kayak' => true 
+  // 'hello' => false | 'aibohphobia' (the fear of palindromes)
+  // Output: true      | true    | false   | true
+
+  function isPalindrome(str) {
+    for(let i = 0; i < str.length/2; i++){
+        if (str[i] != str[str.length-1-i]){
+            return false
+        }
+    }
+    return true
+
+
+  }
+
+  console.log(isPalindrome('kayak'))
+  console.log(isPalindrome('hello'))
+  console.log(isPalindrome('aibohphobia'))
